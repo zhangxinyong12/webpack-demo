fb06d9df0b5d8040d2cd10bf86754482d36b7f51
zhangxinyong <13271150671@wo.cn>
2021-01-10 15:15:10 +0800

    feat: 测试js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
-
+import {a} from './a';
 console.log('webpack  学习');
 console.log('webpack  学习222');
 console.log('webpack  学习3333');
 console.log('webpack  学习4444');
 console.log(333,'reqid');
+
+a(1,2);
